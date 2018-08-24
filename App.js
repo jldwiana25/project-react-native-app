ae499dffa84b9dd6247454bc58806eaf7a86621b
jldwiana25 <juliadwiana2521@gmail.com>
2018-08-24 09:55:02 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Tararam 24</Text>
+        <Text>Nama      :Julia Dwiana </Text>
+        <Text>Kelas     :XI RPL 2     </Text>
+        <Text>Absen     :24           </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
